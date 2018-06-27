9f9139d6c45e14645caf9fe94a7cedc76926b067
James Towle <jalextowle@berkeley.edu>
2018-06-27 18:32:09 -0500

    Added more unit tests for the Admin class and MultipleChoiceIdx

--- education_project/contracts/test_registry/ITestRegistry.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.4.23;
-
-// FIXME - Need to add all of the functionality to this file
-interface ITestRegistry {
-  function init() external;
-}
--- education_project/contracts/test_registry/features/interfaces/Version_Interface.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.4.23;
-
-interface Version_Interface {
-  function totalCompletion(address) external view returns (bool);
-}
